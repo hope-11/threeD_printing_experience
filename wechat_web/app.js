e05341a2c783ac007b34cf07c26519a858166825
Peng Hou <hope-11@163.com>
2017-02-06 17:41:33 +0800

    delete index page

--- wechat_web/app.js
+++ wechat_web/app.js
@@ -4,10 +4,11 @@ App({
     wx.login({
       success: function(res){
         if(res.code){
-          wx.navigateTo({
-            url: 'page/userindex/userindex',
+          wx.getUserInfo({
             success: function(res){
-              // success
+              // 将用户信息存入本地缓存
+              var userInfo=res.userInfo
+              wx.setStorageSync('userInfo', userInfo)
             },
             fail: function() {
               // fail
@@ -25,38 +26,5 @@ App({
         // complete
       }
     })
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    var user = wx.getStorageSync('user') || []
-    if(user.length == 0){
-      
-      wx.setStorageSync('user', globalData.userInfo)
-    }
-
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              //设置用户类型为1
-              that.globalData.userInfo.usertype=1
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
-  globalData:{
-    userInfo:null
   }
 })
--- wechat_web/pages/index/index.js
+++ wechat_web/pages/index/index.js
@@ -1,54 +1,30 @@
-// pages/index/index.js
+//userindex.js
+//获取应用实例
 var app = getApp()
 Page({
   data: {
-    userInfo: wx.getStorageSync('user')
+    userInfo: wx.getStorageSync('userInfo')
   },
-  onLoad:function(options){
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //如果用户类型为1，即为普通用户，则跳转到用户首页
-      if(userInfo.usertype==1){
-        wx.navigateTo({
-          url: '../userindex/userindex',
-          success: function(res){
-            // success
-          },
-          fail: function() {
-            // fail
-          },
-          complete: function() {
-            // complete
-          }
-        })
-        //如果用户类型为0，即为管理员，则跳转到管理员首页
-      }else{
-        wx.navigateTo({
-          url: '../managerindex/managerindex',
-          success: function(res){
-            // success
-          },
-          fail: function() {
-            // fail
-          },
-          complete: function() {
-            // complete
-          }
-        })
-      }
+  //事件处理函数
+  onLoad: function () {
+  },
+  viewUserinfo: function() {
+    wx.navigateTo({
+      url: '../userinfo/userinfo'
     })
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  beginExperience: function() {
+    wx.navigateTo({
+      url: '../experiencing/experiencing',
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
   }
 })
